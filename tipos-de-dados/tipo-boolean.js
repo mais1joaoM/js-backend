3ac4f633bd82ff43cea0f07174c1915752487b8c
mais1joaoM <50158494+mais1joaoM@users.noreply.github.com>
2026-02-19 08:56:03 -0300

    tipo boolean

---
+++ tipos-de-dados/tipo-boolean.js
@@ -0,0 +1,10 @@
+const estudante = 'Joao';
+const estaAprovado = false;
+
+if (estaAprovado){
+    console.log('Parabéns! Aprovado!');
+}else{
+    console.log('Reprovado.')
+}
+
+console.log(Number(false))
